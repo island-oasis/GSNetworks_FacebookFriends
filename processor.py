775143b54d1e5412582fec738cec1d3f6b8a2ddf
Colin <112444601+island-oasis@users.noreply.github.com>
2023-12-10 17:02:23 -0500

    Add files via upload

---
+++ processor.py
@@ -0,0 +1,15 @@
+## Find the K-Cores clusters
+import networkx as nx
+import matplotlib.pyplot as plt 
+G = nx.read_graphml('bfmaier_anonymized_fb_network.graphml')
+nx.draw(G)
+
+cores = nx.core_number(G)
+
+#print(cores)
+
+Gs = {}
+
+for i in 0:10
+    Gs[i] = nx.k_core(G, i, cores)
+    nx.draw(Gs[i])
